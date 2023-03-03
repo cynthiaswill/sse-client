a779d1afce1579b40f65ebfa77d12a1bd3b5bf90
Yi Zhang <90160138+cynthiaswill@users.noreply.github.com>
2023-03-03 12:23:13 +0000

    updates comments

--- client.js
+++ client.js
@@ -4,10 +4,15 @@ const eventSource = new EventSource("http://localhost:8000");
 
 eventSource.onmessage = function (event) {
   console.dir(event);
+  console.log("...  ** Storing new event into DB ** ... \n");
+
+  // all code to do with event or event.data ...
+  // save to SQL db...
 };
 
-eventSource.onerror = function () {
+eventSource.onerror = function reConnectToServer() {
   const eventSource = new EventSource("http://localhost:8000");
-  console.log("Server closed connection");
+  console.log("Error - connection closed! trying to reconnect ...");
+
   eventSource.close();
 };
